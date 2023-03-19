d77630981698b3fa2ab5f85b708e616fd3afaeeb
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2023-03-19 18:12:02 -0500

    Added lowercase, digit, and symbols

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ def shuffle(string):
 
 #Below is based on ASCII Code
 uppercaseLetter1=chr(random.randint(65,90)) #Generate a random Uppercase letter
-uppercaseLetter2=chr(random.randint(65,90))
-uppercaseLetter3=chr(random.randint(65,90))
-uppercaseLetter4=chr(random.randint(65,90))
+lowercaseLetter2=chr(random.randint(97,122)) #Generate a random lowercase letter
+digit1=chr(random.randint(48,57)) #Generate a random digit
+symbol1=chr(random.randint(33,38)) #Generate a random symbol
 
 #Generate password using all the characters, in random order
-password = uppercaseLetter1 + uppercaseLetter2 + uppercaseLetter3 + uppercaseLetter4
+password = uppercaseLetter1 + lowercaseLetter2 + digit1 + symbol1
 password = shuffle(password)
 
 #Ouput
