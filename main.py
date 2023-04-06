1e208acfdbfdc68cfa4683e031be093eebc5bab0
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2023-04-06 17:32:10 -0500

    Added User Input
    Asked how long the password should be and attempted to make a for loop statement according to the password size

--- main.py
+++ main.py
@@ -6,14 +6,16 @@ def shuffle(string):
   return ''.join(tempList)
 
 #Below is based on ASCII Code
-uppercaseLetter1=chr(random.randint(65,90)) #Generate a random Uppercase letter
-lowercaseLetter2=chr(random.randint(97,122)) #Generate a random lowercase letter
+uLetter=chr(random.randint(65,90)) #Generate a random Uppercase letter
+lLetter=chr(random.randint(97,122)) #Generate a random lowercase letter
 digit1=chr(random.randint(48,57)) #Generate a random digit
 symbol1=chr(random.randint(33,38)) #Generate a random symbol
 
 #Generate password using all the characters, in random order
-password = uppercaseLetter1 + lowercaseLetter2 + digit1 + symbol1
-password = shuffle(password)
+password = uLetter or lLetter or digit1 or symbol1
 
 #Ouput
-print(password)
+pwSize = int(input("How many characters should this passord be?: "))
+
+for x in range(pwSize):
+    print (password)
