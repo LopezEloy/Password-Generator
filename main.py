e32c9750667bb924007b6c5253a53f6cae295d0e
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2023-04-11 02:40:24 -0500

    Removed unnecessary code
    Removed any test print statement and switched around some orders for consistency

--- main.py
+++ main.py
@@ -1,44 +1,37 @@
 import random #Import the random library
 
-#Below is based on ASCII Code
+#Getting User input for PW details
+pwSize = int(input("How long should this password be? Give a value: "))
+pwLet = int(input("How many letters?: "))
+pwDig = int(input("How many digits: "))
+pwSym = int(input("How many symbols?: "))
 
+print(f"Generating a password of length {pwSize} with {pwLet} letter(s), {pwDig} digit(s), and {pwSym} symbol(s)")
 
-#Keep track of random generated individual passwords
-totSym = []
+#Keep track of random generated individual passwords in arrays
+totLet = []
 totDig = []
-totChar = []
+totSym = []
 totPW = []
 
-#Ouput
-pwSize = int(input("How long should this password be? Give a value: "))
-pwSym = int(input("How many symbols?: "))
-pwDig = int(input("How many digits: "))
-pwChar = int(input("How many characters?: "))
-
-print(f"Generating a password of length {pwSize} with {pwSym} symbol(s), {pwDig} digit(s), and {pwChar} character(s)")
-
-for x in range(0, pwSym):
-    symbol1 = chr(random.randint(33,38)) #Generate a random symbol
-    totSym.append(symbol1)
-#print(totSym) This is to test if the symbols are different
+#Creating for loops to randomize letters, digits, and symbols
+for x in range(0, pwLet):
+    uLetter=chr(random.randint(65,90)) #Generate a random Uppercase letter
+    lLetter=chr(random.randint(97,122)) #Generate a random lowercase letter
+    letters = uLetter or lLetter
+    totLet.append(letters)
 
 for x in range(0, pwDig):
     digit1 = chr(random.randint(48,57)) #Generate a random digit
     totDig.append(digit1)
 
-for x in range(0, pwChar):
-    uLetter=chr(random.randint(65,90)) #Generate a random Uppercase letter
-    lLetter=chr(random.randint(97,122)) #Generate a random lowercase letter
-    letters = uLetter or lLetter
-    totChar.append(letters)
+for x in range(0, pwSym):
+    symbol1 = chr(random.randint(33,38)) #Generate a random symbol
+    totSym.append(symbol1)
 
-totPW = totSym + totDig + totChar
+#Joining all created letters, digits, and symbols
+totPW = totSym + totDig + totLet
+
 a = list(totPW)
-
-print(''.join(totSym))
-print(''.join(totDig))
-print(''.join(totChar))
-#print(''.join(totPW)) Prints the combined password together
 random.shuffle(a)
-b = ''.join(a)
-print(b)
+print(''.join(a))
