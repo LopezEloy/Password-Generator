313af9b2b87e962e3c0c8476171a5d9697390069
Eloy Lopez <104275355+LopezEloy@users.noreply.github.com>
2023-04-11 02:20:36 -0500

    Added digits, Characters; Made full PW
    Added the digits and characters (for loops) and combined them all to create a full completed password
    Added a list and shuffle functionality to randomize the order of the final password

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import random #Import the random library
 
 #Below is based on ASCII Code
-uLetter=chr(random.randint(65,90)) #Generate a random Uppercase letter
-lLetter=chr(random.randint(97,122)) #Generate a random lowercase letter
-digit1=chr(random.randint(48,57)) #Generate a random digit
+
 
 #Keep track of random generated individual passwords
 totSym = []
 totDig = []
 totChar = []
+totPW = []
 
 #Ouput
 pwSize = int(input("How long should this password be? Give a value: "))
@@ -21,6 +20,25 @@ print(f"Generating a password of length {pwSize} with {pwSym} symbol(s), {pwDig}
 for x in range(0, pwSym):
     symbol1 = chr(random.randint(33,38)) #Generate a random symbol
     totSym.append(symbol1)
-print(totSym)
+#print(totSym) This is to test if the symbols are different
+
+for x in range(0, pwDig):
+    digit1 = chr(random.randint(48,57)) #Generate a random digit
+    totDig.append(digit1)
+
+for x in range(0, pwChar):
+    uLetter=chr(random.randint(65,90)) #Generate a random Uppercase letter
+    lLetter=chr(random.randint(97,122)) #Generate a random lowercase letter
+    letters = uLetter or lLetter
+    totChar.append(letters)
+
+totPW = totSym + totDig + totChar
+a = list(totPW)
 
 print(''.join(totSym))
+print(''.join(totDig))
+print(''.join(totChar))
+#print(''.join(totPW)) Prints the combined password together
+random.shuffle(a)
+b = ''.join(a)
+print(b)
